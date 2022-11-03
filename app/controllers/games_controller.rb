5b3b7e113668774c35a55112b35ff065b21c2dd0
brice moulin <moulin_brice@hotmail.com>
2022-11-03 16:41:43 -0400

    rails longest word last

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -9,20 +9,36 @@ class GamesController < ApplicationController
     10.times{
       @letters << letter_tab[rand(0..letter_tab.length)]
     }
+    @sav_letters = @letters
   end
 
   def score
-    @word=params[:word]
-    url = "https://wagon-dictionary.herokuapp.com/#{@word}"
-    word_serialized = URI.open(url).read
-    back_word = JSON.parse(word_serialized)
+    @answer = ""
+    @word = params[:word]
+    @letters = params[:letters]
 
+     # Test1 Le mot ne peut pas être créé à partir de la grille d’origine.
+     goodWord = true
+     @word.split("").each do |letter|
+       @word.include?(letter) ? goodWord = true : goodWord = false
+       break letter if goodWord = false
+     end
 
+    if goodWord
+      url = "https://wagon-dictionary.herokuapp.com/#{@word}"
+      word_serialized = URI.open(url).read
+      back_word = JSON.parse(word_serialized)
 
-    puts "#{user["name"]} - #{user["bio"]}"
-
+      if !back_word[:found]
+        # Le mot est valide d’après la grille, mais ce n’est pas un mot anglais valide.
+        @answer = "Sorry but <strong>#{@word}</strong> does not seem to be a valid English word..."
+      else
+        # Le mot est valide d’après la grille et est un mot anglais valide.
+        @answer = "<strong>Congratulation!</strong> #{@word} is a valid English word!"
+      end
+    else
+      # Test1 Le mot ne peut pas être créé à partir de la grille d’origine.
+      @answer = "Sorry but <strong>#{@word}</strong> can't be build out of #{@letters.split("").join(",")}"
+    end
   end
-
-
-
 end
